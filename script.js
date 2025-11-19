7b0db45a7431e0cf9e074613f567f2b22548fb5a
Pabla Vittar <148217553+CCCHRIS-CHRIS@users.noreply.github.com>
2025-11-19 16:56:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        tabButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        const target = button.getAttribute("data-target");
+
+        tabContents.forEach(content => {
+            content.classList.remove("active");
+            if (content.id === target) {
+                content.classList.add("active");
+            }
+        });
+    });
+});
